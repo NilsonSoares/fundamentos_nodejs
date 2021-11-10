171e13a9a93cb04990edd61560f335d51756977f
Nilson Soares <nilsonsoares2011@gmail.com>
2021-11-10 01:13:51 -0300

    Testa as rotas que recebem parâmetros nas requisições

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ const express = require("express");
 
 // Instancia o express
 const app = express();
+
+// Indica para o express que o Body Param pode ser do tipo JSON
+app.use(express.json());
+
 /**
  * GET - Buscar uma informação dentro do servidor
  * POST - Inserir uma informação no servidor
  * PUT - Alterar uma informação no servidor
  * PATH - Alterar uma informação específica
  * DELETE - Deletar uma iformação no servidor
+ */
+
+/**
+ * TIPOS DE PARÂMETROS
+ * 
+ * Route Params - Identificar um recurso para buscar / editar / deletar
+ * Query Params - Paginação / Filtro
+ * Body Params - Ojetos enviados para inserção / alteração (JSON)
  */
 // Cria uma rota GET
 app.get("/", (request, response) => {
@@ -18,15 +30,21 @@ app.get("/", (request, response) => {
 });
 
 // Cria uma rota GET
-app.get("/couses", (request, response) => {
+app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query);
   return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 // Cria uma rota POST
 app.post("/courses", (request, response) => {
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+  const body = request.body;
+  console.log(body);
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 // Cria uma rota PUT com o id do recurso a ser alterado
 app.put("/courses/:id", (request, response) => {
+  const params = request.params;
+  console.log(params);
     return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 // Cria uma rota PATH com o id do recurso a ser alterado
